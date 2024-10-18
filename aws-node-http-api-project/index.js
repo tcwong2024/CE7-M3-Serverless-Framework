1b7f00d866179359298a4ce4e554e97b08b18c48
tcwong2024 <tcwong99@hotmail.com>
2024-10-18 11:52:54 +0800

    Initial commit

--- aws-node-http-api-project/index.js
+++ aws-node-http-api-project/index.js
@@ -1,6 +1,6 @@
 module.exports.handler = async (event) => {
     // write hello world to the logs
-    console.log("Hello World");
+    console.log("Hello World and WTC");
     // assign a value to a variable
     let message = "Go WTC Serverless v4.0! Your function executed successfully!";
 
